8a7e2d42e15b88e76c34d4b21c457f25d59ceed2
Osni Baumgartner Junior <osnijunior.x3@gmail.com>
2024-11-20 15:19:44 -0300

    First commit

---
+++ OTELTraces/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace OTELTraces
+{
+    public class App
+    {
+        // Creates the Tracer
+        private static readonly ActivitySource Tracer = new ActivitySource("csharp.console.traces");
+
+        public static void Main(string[] args)
+        {
+            // Configures the SDK, exporting to a local running Collector
+            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddSource(Tracer.Name)
+                .SetSampler(new AlwaysOnSampler())
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("csharp.console.traces"))
+                .AddOtlpExporter(opts =>
+                {
+                    opts.Endpoint = new Uri("http://collector-otel-recipes:4317");
+                })
+                .Build();
+
+            // Start a span with a tag
+            using var activity = Tracer.StartActivity("Hello Appoena!");
+            activity?.SetTag("data", "dog");
+        }
+    }
+}
